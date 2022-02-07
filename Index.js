464e2926b1b1a36320d4216445f5ee4c22c5e533
SaifSushi <alushisaif@gmail.com>
2022-02-07 17:00:22 -0600

    working on controller bug

--- Index.js
+++ Index.js
@@ -1,18 +1,22 @@
-const express = require('express');
+require("./connection")
+const express = require('express')
 const cors = require('cors')
 
 const app = express();
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 8000)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 app.get('/', (req, res) => {
-	res.redirect('/login');
-});
+	res.redirect('/grouper')
+})
+
+const userController = require("./controllers/userController")
+app.use("/grouper/signup", userController)
 
 
 app.listen(app.get('port'), () => {
-	console.log(`✅ PORT: ${app.get('port')} 🌟`);
-});
+	console.log(`✅ PORT: ${app.get('port')} 🌟`)
+})
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,3 +2,41 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 
+const bcrypt = require('bcrypt')
+
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const password = await bcrypt.hash(req.body.password, 10)
+    const email = req.body.email
+    const name = req.body.name
+    const user = await User.create({ email, password, name })
+    res.status(201).json(user)
+  } catch (error) {
+    return next(error)
+  }
+});
+// router.post('/signup', (req, res, next) => {
+//     bcrypt
+//       .hash(req.body.password, 10)
+//       // return a new object with the email and hashed password
+//       .then(hash =>
+//         // when returning an object with fat arrow syntax, we
+//         // need to wrap the object in parentheses so JS doesn't
+//         // read the object curly braces as the function block
+//         ({
+//           email: req.body.email,
+//           name: req.body.name,
+//           password: hash
+//         })
+//       )
+//       // create user with provided email and hashed password
+//       .then(user => User.create(user))
+//       // send the new user object back with status 201, but `hashedPassword`
+//       // won't be sent because of the `transform` in the User model
+//       .then(user => res.status(201).json(user))
+//       // pass any errors along to the error handler
+//       .catch(next);
+//   });
+
+module.exports = router
