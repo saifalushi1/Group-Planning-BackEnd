82534574d11500337369eba38b5ea3edc6e274a6
SaifSushi <alushisaif@gmail.com>
2022-02-15 14:07:28 -0600

    testing deployment

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express();
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT )
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
